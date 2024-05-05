f8819a84d2c05779fb2edd5cb614ec7107beece3
unknown <dejanmaric259@gmail.com>
2024-05-05 11:54:09 +0200

    1 second request logic

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,28 +1,41 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './Search.module.css'
 import {Icon} from '@iconify/react'
 import { useMovieContext } from '../../contexts/MoviesContext'
+
+
 function Search() {
-
-  interface inputInterface{
-    currentInput:string;
-  }
-  const searchInputRef = useRef<HTMLInputElement>(null)
   const {searchMovies,getMovies,getShows,tab} = useMovieContext()
-
+  const [delaySearch, setDelaySearch] = useState();
+  const [inputValue,setInputValue] = useState('')
+  
+  
+  
+  //handle search function
   function handleSearch(){
-    let currentInput = searchInputRef.current?.value
-    
-    if(currentInput  && currentInput?.length >= 3){
-      searchMovies(currentInput)
+    if(inputValue  && inputValue?.length >= 3){
+      searchMovies(inputValue)
     }else{
        tab === 'shows' ? getShows() :getMovies()
     }
   }
+  
+   useEffect(() => {
+      if(inputValue.length){
+        const timer = setTimeout(()=>{
+          console.log('search started after 1 second')
+          handleSearch()
+        },1000)
+        
+      //clear the timer
+      return () => clearTimeout(timer);
+      }
+
+  }, [inputValue])
   return (
     <div className={styles.inputContainer}>
       <Icon className={styles.inputIcon} icon="material-symbols:search" width="24" height="24"  style={{color: '#fff'}} />
-      <input onChange={()=>handleSearch()} className={styles.inputField} placeholder='Search For Movies' ref={searchInputRef}/>
+      <input onChange={(e)=>setInputValue(e.target.value)} className={styles.inputField} placeholder='Search For Movies'/>
     </div>
   )
 }
